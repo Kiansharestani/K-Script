b97380cd31ef5f825db15b10479fa0db480f29b5
Kiansharestani <ksharestani@gmail.com>
2025-06-02 16:28:29 +0330

    Add files via upload

---
+++ K_Script_0.1_Beta.py
@@ -0,0 +1,11 @@
+if not __name__ == "__main__" :
+    def init() :
+        import time
+    def prn(text) :
+        print(text)
+    def inp(text) :
+        return input(text)
+else :
+    import time
+    print("This Code is not for User Use. This Program is The K_Script Compiler. Please Don't Edit or Delete This File.")
+    time.sleep(5)
